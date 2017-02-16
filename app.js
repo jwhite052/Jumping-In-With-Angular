237c3ab300f0b72c1dec3255fcf3e3a646490f1f
Jashua R. White <jashuawhite@gmail.com>
2017-02-15 23:16:19 -0500

    Starting lesson

---
+++ app.js
@@ -0,0 +1,16 @@
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', [
+'$scope',
+function($scope){
+  $scope.test = 'Hello world!';
+
+  $scope.posts = [
+      {title: 'post 1', upvotes: 3},
+      {title: 'post 2', upvotes: 4},
+      {title: 'post 3', upvotes: 66},
+      {title: 'post 4', upvotes: 7},
+      {title: 'post 5', upvotes: 5}
+  ];
+
+}]);
