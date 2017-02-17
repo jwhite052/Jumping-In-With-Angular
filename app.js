37e4fd5d1bd4b029454b077ad0854d7fa47119d4
Jashua R. White <jashuawhite@gmail.com>
2017-02-16 21:43:30 -0500

    Added scope functions for upvoting and adding posts with links.

--- app.js
+++ app.js
@@ -8,9 +8,28 @@ function($scope){
   $scope.posts = [
       {title: 'post 1', upvotes: 3},
       {title: 'post 2', upvotes: 4},
-      {title: 'post 3', upvotes: 66},
+      {title: 'post 3', upvotes: 66666},
       {title: 'post 4', upvotes: 7},
       {title: 'post 5', upvotes: 5}
   ];
+
+  $scope.addPost = function() {
+    // return if title is blank
+    if(!$scope.title || $scope.title === '') { return; }
+
+    // add title to posts list
+    $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+    });
+  };
+
+  $scope.title = '';
+  $scope.link = '';
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  }
 
 }]);
